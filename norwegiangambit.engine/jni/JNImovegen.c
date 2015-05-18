2e529d4a00cf1a9af61ebf82a33700c257c5639f
Per Digre <psd@esito.no>
2015-05-18 15:55:08 +0200

    Castfree magics

--- norwegiangambit.engine/jni/JNImovegen.c
+++ norwegiangambit.engine/jni/JNImovegen.c
@@ -6,21 +6,29 @@ JNIEXPORT void JNICALL Java_jni_JNImovegen_movegen(JNIEnv *env, jobject thisObj)
    printf("Hello World!\n");
    return;
 }
-
-jint *offsets;
-jlong *magics;
+#define NUM_OFFSETS 64*4
+#define NUM_MAGICS 90624+128+4800+128
+int offsets[NUM_OFFSETS];
+unsigned long long magics[NUM_MAGICS];
 
 JNIEXPORT void JNICALL Java_jni_JNImovegen_copyMagic(JNIEnv *env, jobject thisObj, jintArray joffsets, jlongArray jmagics) {
-	offsets = (*env)->GetIntArrayElements(env, joffsets, NULL);
-	magics = (*env)->GetLongArrayElements(env, jmagics, NULL);
+	jint *toffsets = (*env)->GetIntArrayElements(env, joffsets, NULL);
+	int i=0;
+	for(i=0;i<NUM_OFFSETS;i++){
+		offsets[i]=(int) toffsets[i];
+	}
+	jlong *tmagics = (*env)->GetLongArrayElements(env, jmagics, NULL);
+	for(i=0;i<NUM_MAGICS;i++){
+		magics[i]=(unsigned long long) tmagics[i];
+	}
 	return;
 }
 
 JNIEXPORT jlong JNICALL Java_jni_JNImovegen_magicAtks(JNIEnv *env, jobject thisObj, jlong occupied, jint i){
-	int offset = (int) offsets[i];
-	int bits = (int) offsets[i+1];
-	unsigned long long magic = (unsigned long long) magics[offset-2];
-	unsigned long long mask = (unsigned long long) magics[offset-1];
+	int offset = offsets[i];
+	int bits = offsets[i+1];
+	unsigned long long magic = magics[offset-2];
+	unsigned long long mask = magics[offset-1];
 	return magics[offset + (int)(((occupied & mask) * magic) >> bits)];
 }
 
