ce4aa4bc56d5487dbe35d7571de6218817e8764d
Per Digre <psd@esito.no>
2015-06-17 15:31:45 +0200

    Images

--- jschess/ui.js
+++ jschess/ui.js
@@ -2,16 +2,24 @@
  * 
  */
 
-document.write(createBoard());
-
 function createBoard(){
-	var txt="<table>";
+	var txt="";
+	var ch="<tr>";
+	for(c=0;c<10;c++)
+		ch+='<td class="hdr">'+" ABCDEFGH ".charAt(c)+'</td>';
+	ch+='</tr>';
 	for(r=0;r<8;r++){
-		txt+="<tr>";
+		var rh='<td class="hdr">'+"87654321".charAt(r)+'</td>';
+		txt+='<tr>'+rh;
 		for(c=0;c<8;c++){
-			txt+='<td class="'+((c+r)%2==1?"blk":"wht") +'"></td>';
+			txt+='<td class="'+((c+r)%2==1?"blk":"wht") +'" id="sq'+((7-r)*8+c)+'"></td>';
 		}
-		txt+="</tr>";
+		txt+=rh+'</tr>';
 	}
-	return txt+"</table>";
+	return '<table id="brd" class="brd">'+ch+txt+ch+'</table>';
+}
+
+function setFEN(FEN){
+	var sq=document.getElementById("sq1");
+	sq.className=sq.className+" K";
 }
